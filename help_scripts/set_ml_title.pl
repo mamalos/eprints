150cafeaf6bef40d0e83de21e34c0b2d98ca8fcc
George Mamalakis <mamalos@eng.auth.gr>
2016-07-07 13:17:34 +0300

    added some comments on how to make set_ml_title.pl script to be sused to change ml_abstract as well. Comments explain that by default it sets English and Greek title.

--- help_scripts/set_ml_title.pl
+++ help_scripts/set_ml_title.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/perl -I /usr/share/eprints3/perl_lib
+# No liability for the contents of this document can be accepted. Use the 
+# concepts, examples and information at your own risk. There may be errors, 
+# omissions, and inaccuracies that could cause you to lose data, harm your 
+# system, or induce involuntary electrocution, so proceed with appropriate 
+# caution. The author takes no responsibility for any damages, incidental or 
+# otherwise.
+
+# This script copies the title field of each eprint in ml_title field. You can
+# replace title with abstract and the same script will copy the contents of 
+# the abstract field in ml_abstract.
+
+# !!! WARNING !!!
+# The values are copied in both en and el languages. Change this behaviour to
+# reflect your own configuration.
+
 use EPrints;
 my $DEBUG = 0;
 die "usage: $0 <reponame> [dataset]\n" if @ARGV < 1;
